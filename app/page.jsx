8754fd11a4d16f80691abb9395c759192dc30e0a
sandor <sandor.den@gmail.com>
2025-03-19 23:56:20 +0000

    Initalized the tailwind theme

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+	return (
+		<div className="flex flex-col lg:flex-row justify-center items-center gap-6">
+			<div className="w-1/2">
+				<h3>The quick brown fox jumps over the lazy dog.</h3>
+			</div>
+			<div className="w-1/2">
+				<p>The quick brown fox jumps over the lazy dog.</p>
+			</div>
+		</div>
+	);
+}
